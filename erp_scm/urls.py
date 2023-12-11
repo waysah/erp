a41d881aba991a3792f5eaed4f72430b211245ff
Simon Kamau <simonkamau690@gmail.com>
2023-12-11 17:23:45 +0300

    supplier urls

--- erp_scm/urls.py
+++ erp_scm/urls.py
@@ -1,7 +1,19 @@
 from django.http import HttpResponse
 from django.urls import path 
 from . import views 
+from . views import SupplierCreateView, SupplierDeleteView
+from . views import SupplierDetailView, SupplierListView
+from . views import SupplierUpdateView
 
 urlpatterns = [
-    path('', views.erp_scm, name="erp_scm")
+    path('', views.erp_scm, name="erp_scm"),
+
+    # Urls for suppliers
+    path('supplier/', SupplierListView.as_view(), name='services'),
+    path('supplier/new/', SupplierCreateView.as_view(), name='supplier_create'),
+    path('supplier/<int:pk>/edit/', SupplierUpdateView.as_view(), name='supplier_update'),
+    path('supplier/<int:pk>/', SupplierDetailView.as_view(), name='supplier_detail'),
+    path('supplier/<int:pk>/delete/', SupplierDeleteView.as_view(), name='supplier_delete'),
+
+    
 ]
