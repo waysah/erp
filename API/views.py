5d79b1d32f6e9f436736582f8c0cba43cbcb3657
Simon Kamau <simonkamau690@gmail.com>
2023-12-11 14:38:14 +0300

    api generic views

--- API/views.py
+++ API/views.py
@@ -14,11 +14,23 @@ class ManagerRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset  = Manager.objects.all()
     serializer_class = ManagerSerializer
 
+class ManagerListCreateView(generics.ListCreateAPIView):
+    queryset  = Manager.objects.all()
+    serializer_class = ManagerSerializer
+
 class EmployeesRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Employees.objects.all()
     serializer_class = EmployeeSerializer
 
+class EmployeesListCreateView(generics.ListCreateAPIView):
+    queryset = Employees.objects.all()
+    serializer_class = EmployeesSerializer
+
 class employee_initializerRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = employee_initializer.objects.all()
+    serializer_class = employee_initializerSerializer
+
+class employee_initializerListCreateView(generics.ListCreateAPIView):
     queryset = employee_initializer.objects.all()
     serializer_class = employee_initializerSerializer
 
@@ -26,7 +38,15 @@ class commision_salaryRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAP
     queryset = Commision_saraly.objects.all()
     serializer_class = Commision_saralySerializer
 
+class commision_salaryListCreateView(generics.ListCreateAPIView):
+    queryset  = Commision_saraly.objects.all()
+    serializer_class = Commision_saralySerializer
+
 class Fixed_saralyRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Fixed_saraly.objects.all()
+    serializer_class = Fixed_saralySerializer
+
+class Fixed_saralyListCreateView(generics.ListCreateAPIView):
     queryset = Fixed_saraly.objects.all()
     serializer_class = Fixed_saralySerializer
 
@@ -34,19 +54,39 @@ class allowancesRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView)
     queryset = allowances.objects.all()
     serializer_class = allowancesSerializer
 
+class allowancesListCreateView(generics.ListCreateAPIView):
+    queryset = allowances.objects.all()
+    serializer_class = allowancesSerializer
+
 class deductionRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = deductions.objects.all()
-    serializer_class = allowancesSerializer
+    serializer_class = deductionsSerializer
+
+class deductionListCreateView(generics.ListCreateAPIView):
+    queryset = deductions.objectsa.all()
+    serializer_class = deductionsSerializer
 
 class salary_detailsRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset  = salary_details.objects.all()
+    serializer_class = salary_detailsSerializer
+
+class salary_detailsListCreateView(generics.ListCreateAPIView):
+    queryset = salary_details.objects.all()
     serializer_class = salary_detailsSerializer
 
 class job_typeRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset  = job_type.objects.all()
     serializer_class = job_typeSerializer
 
+class job_typeListCreateView(generics.ListCreateAPIView):
+    queryset = job_type.objects.all()
+    serializer_class = job_typeSerializer
+
 class SelectDepartmentRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = SelectDepartment.objects.all()
+    serializer_class = SelectDepartmentSerializer
+
+class SelectDepartmentListCreateView(generics.ListCreateAPIView):
     queryset = SelectDepartment.objects.all()
     serializer_class = SelectDepartmentSerializer
 
@@ -54,7 +94,15 @@ class PayrollSummaryRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIV
     queryset = PayrollSummary.objects.all()
     serializer_class = PayrollSummarySerializer
 
+class PayrollSummaryListCreateView(generics.ListCreateAPIView):
+    queryset = PayrollSummary.objects.all()
+    serializer_class = PayrollSummarySerializer
+
 class AdvanceSalaryRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = AdvanceSalary.objects.all()
+    serializer_class = AdvanceSalarySerializer
+
+class AdvanceSalaryListCreateView(generics.ListCreateAPIView):
     queryset = AdvanceSalary.objects.all()
     serializer_class = AdvanceSalarySerializer
 
@@ -62,7 +110,15 @@ class overtimeRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = overtime.objects.all()
     serializer_class = overtimeSerializer
 
+class overtimeListCreateView(generics.ListCreateAPIView):
+    queryset = overtime.objects.all()
+    serializer_class = overtimeSerializer
+
 class employeeAwardRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = employeeAward.objects.all()
+    serializer_class = employeeAwardSerializer
+
+class employeeAwardListCreateView(generics.ListCreateAPIView):
     queryset = employeeAward.objects.all()
     serializer_class = employeeAwardSerializer
 
@@ -70,7 +126,15 @@ class addExpensesRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView
     queryset = addExpenses.objects.all()
     serializer_class = addExpensesSerializer
 
+class addExpensesListCreateView(generics.ListCreateAPIView):
+    queryset = addExpenses.objects.all()
+    serializer_class = addExpensesSerializer
+
 class DepartmentRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Department.objects.all()
+    serializer_class = DepartmentSerializer
+
+class DepartmentListCreateView(generics.ListCreateAPIView):
     queryset = Department.objects.all()
     serializer_class = DepartmentSerializer
 
@@ -78,7 +142,15 @@ class first_departmentRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAP
     queryset = first_department.objects.all()
     serializer_class = first_departmentSerializer
 
+class first_departmentListCreateView(generics.ListCreateAPIView):
+    queryset = first_department.objects.all()
+    serializer_class = first_departmentSerializer
+
 class DailyWorkRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = DailyWork.objects.all()
+    serializer_class = DailyWorkSerializer
+
+class DailyWorkListCreateView(generics.ListCreateAPIView):
     queryset = DailyWork.objects.all()
     serializer_class = DailyWorkSerializer
 
@@ -86,11 +158,23 @@ class EmployeeRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
 
+class EmployeeListCreateView(generics.ListCreateAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
 class TransactionRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
 
+class TransactionListCreateView(generics.ListCreateAPIView):
+    queryset = Transcation.objects.all()
+    serializer_class = TransactionSerializer
+
 class commision_rateRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = commision_rate.objects.all()
+    serializer_class = commision_rateSerializer
+
+class commision_rateListCreateView(generics.ListCreateAPIView):
     queryset = commision_rate.objects.all()
     serializer_class = commision_rateSerializer
 
@@ -98,7 +182,15 @@ class commission_templatesRetrieveUpdateDestroyView(generics.RetrieveUpdateDestr
     queryset = commission_templates.objects.all()
     serializer_class = commission_templatesSerializer
 
+class commission_templatesListCreateView(generics.ListCreateAPIView):
+    queryset = commission_templates.objects.all()
+    serializer_class = commission_templatesSerializer
+
 class nextofkinRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = nextofkin.objects.all()
+    serializer_class = nextofkinSerializer
+
+class nextofkinListCreateView(generics.ListCreateAPIView):
     queryset = nextofkin.objects.all()
     serializer_class = nextofkinSerializer
 
@@ -106,11 +198,23 @@ class first_employeesRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPI
     queryset = first_employees.objects.all()
     serializer_class = first_employeesSerializer
 
+class first_employeesListCreateView(generics.ListCreateAPIView):
+    query = first_employees.objects.all()
+    serializer_class = first_employeesSerializer
+
 class payslipRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = payslip.objects.all()
     serializer_class = payslipSerializer
 
+class payslipListCreateView(generics.ListCreateAPIView):
+    queryset = payslip.objects.all()
+    serializer_class = payslipSerializer
+
 class financialreportRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = financialreport.objects.all()
+    serializer_class = financialreportSerializer
+
+class financialreportListCreateView(generics.ListCreateAPIView):
     queryset = financialreport.objects.all()
     serializer_class = financialreportSerializer
 
@@ -122,7 +226,15 @@ class department_employee_viewRetrieveUpdateDestroyView(generics.RetrieveUpdateD
     queryset = department_employee_view.objects.all()
     serializer_class = department_employee_viewSerializer
 
+class department_employee_viewListCreateView(generics.ListCreateAPIView):
+    queryset = department_employee_view.objects.all()
+    serializer_class = department_employee_viewSerializer
+
 class debitRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = debit.objects.all()
+    serializer_class = debitSerializer
+
+class debitListCreateView(generics.ListCreateAPIView):
     queryset = debit.objects.all()
     serializer_class = debitSerializer
 
@@ -130,11 +242,23 @@ class system_updateRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIVi
     queryset  = system_update.objects.all()
     serializer_class = system_updateSerializer
 
+class system_updateListCreateView(generics.ListCreateAPIView):
+    queryset = system_update.objects.all()
+    serializer_class = system_updateSerializer
+
 class payement_gatewayRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = payement_gateway.objects.all()
+    serializer_class = payement_gatewaySerializer
+
+class payement_gatewayListCreateView(generics.ListCreateAPIView):
     queryset = payement_gateway.objects.all()
     serializer_class = payement_gatewaySerializer
 
 class system_paymentRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = system_payment.objects.all()
     serializer_class = system_paymentSerializer
-    
+
+class system_paymentListCreateView(generics.ListCreateAPIView):
+    queryset = system_payment.objects.all()
+    serializer_class = system_paymentSerializer
+
